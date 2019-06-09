ef50e2c3cc6544f13be718e39f337d2dd3c3ede5
Alonso Rivel <alonso.rivel@gmail.com>
2019-06-09 14:08:50 -0400

    Set express middleware and sample data

--- build/server.js
+++ build/server.js
@@ -1,8 +1,36 @@
+// Import dependencies
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+// Set express app
 const app = express();
 
+// Mimic database data
+const users = [
+  {
+    email: "test@ns8.com",
+    password: "passwordIsPizza",
+    phone: "333-222-1111"
+  }
+];
+
+// Set Helmet for app security
+app.use(helmet());
+
+// Set bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// Enable CORS for all requests
+app.use(cors());
+
+// Set morgan to log HTTP requests
+app.use(morgan("combined"));
+
 app.get("/", (req, res, next) => {
-  res.send("Users API");
+  res.send(users);
 });
 
 app.listen(process.env.PORT || 3001, () => {
